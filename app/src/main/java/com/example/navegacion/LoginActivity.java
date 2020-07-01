9c9dd8a50a8ec2d9afb1871f1e06d78ecc13be74
Eduardo Delgado <eduardoo.andrees@gmail.com>
2020-07-01 17:10:39 -0400

    pantalla login con su actividad, modificación string.xml se añaden mensajes del login

---
+++ app/src/main/java/com/example/navegacion/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.navegacion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText eRut, ePassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        eRut = findViewById(R.id.eRut);
+        ePassword= findViewById(R.id.ePass);
+
+    }
+    public void login(View viex){
+        String sRut = eRut.getText().toString();
+        String sPass = ePassword.getText().toString();
+        String sMensaje= "";
+
+        //validar ingreso datos
+        if(sRut.isEmpty() && sPass.isEmpty()){ // V^V=V
+            sMensaje = String.valueOf(R.string.string_msg_datos);
+            mensajes(sMensaje);
+        }else if(sRut.isEmpty()){
+            Toast.makeText(this, R.string.string_msg_rut, Toast.LENGTH_SHORT).show();
+        }else if(sPass.isEmpty()){
+            Toast.makeText(this, R.string.string_msg_pass, Toast.LENGTH_SHORT).show();
+        }else{
+            //validar datos correctos
+            if(!sRut.toLowerCase().equals(R.string.string_rut)){
+                Toast.makeText(this, "Rut Incorrecto", Toast.LENGTH_SHORT).show();
+            }else if(!sPass.equals(R.string.string_password)){
+                Toast.makeText(this, "Password Incorrecto", Toast.LENGTH_SHORT).show();
+            }else{
+                sMensaje = "Login correcto";
+                //Llamando a la función , pasando un parametro
+                mensajes(sMensaje);
+                // https://codeshare.io/218e1j
+                Intent pantalla2 = new Intent(this, MainActivity.class);
+                startActivity(pantalla2);
+            }
+
+
+        }
+
+    }
+    //recibir y mostrar todos los mensajes
+    public void mensajes(String mensaje){
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+}
